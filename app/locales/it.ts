319a7a8594dbe3f851a0fa3da8b1ae92a2261ed6
Jean <87219219+jean-raffe@users.noreply.github.com>
2024-11-09 13:47:50 +0800

    Update it.ts

--- app/locales/it.ts
+++ app/locales/it.ts
@@ -306,14 +306,6 @@ const it: PartialLocaleType = {
     },
 
     Access: {
-      SaasStart: {
-        Title: "Usa NextChat AI",
-        Label: "(La soluzione più conveniente)",
-        SubTitle:
-          "Mantenuto ufficialmente da NextChat, pronto all'uso senza configurazione, supporta i modelli più recenti come OpenAI o1, GPT-4o e Claude-3.5",
-        ChatNow: "Chatta ora",
-      },
-
       AccessCode: {
         Title: "Password di accesso",
         SubTitle: "L'amministratore ha abilitato l'accesso criptato",
